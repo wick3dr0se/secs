9e4f664a6a9b93bc766a3ed3ea6d1d7e26909ea7
wick3dr0se <wick3dr0se@protonmail.com>
2025-01-29 10:59:45 -0500

    add player velocity and mutate both components via `query()`

--- examples/player_move.rs
+++ examples/player_move.rs
@@ -5,18 +5,29 @@ struct Position {
     y: i32
 }
 
+struct Velocity {
+    x: i32,
+    y: i32
+}
+
 impl Component for Position {}
+impl Component for Velocity {}
 
 fn main() {
     let mut world = World::default();
     let player = world.spawn();
 
     world.attach(player, Position { x: 0, y: 0 });
+    world.attach(player, Velocity { x: 0, y: 0 });
 
-    for (_entity,(pos,)) in world.query::<(&mut Position,)>() {
-        pos.x += 1;
-        pos.y += 1;
-
-        println!("Player position: ({}, {})", pos.x, pos.y);
+    for (entity,(pos, vel)) in world.query::<(&mut Position, &mut Velocity)>() {
+        if player == entity { // only entity in world currently
+            vel.x = 1;
+            vel.y = -1;
+            pos.x += vel.x;
+            pos.y += vel.y;
+    
+            println!("Player position: ({}, {})", pos.x, pos.y);
+        }
     }
 }
