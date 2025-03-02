6aac803943f664c1973c8593e7225a4475ea45ce
Oli Scherer <github@oli-obk.de>
2025-03-02 16:04:02 +0100

    Fix CI due to copy paste bug

--- tests/system.rs
+++ tests/system.rs
@@ -8,6 +8,10 @@ fn remove() {
     fn boom(_: &World) {
         panic!()
     }
+    // Miri will assume every time you mention a function you could have a new address
+    // While this is bogus in many cases, it's the only way to catch the rare cases where
+    // it's an issue. So we do this provenance preserving deduplication by eagerly creating a pointer.
+    let boom = boom as fn(&World);
     world.add_system(boom, ExecutionMode::Serial);
     assert!(std::panic::catch_unwind(AssertUnwindSafe(|| world.run_systems())).is_err());
     world.remove_system(boom);
