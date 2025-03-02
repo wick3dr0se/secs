f5a9a83e1fe6935987ef838e6a309b0b8a8fcd4b
Oli Scherer <github@oli-obk.de>
2025-03-02 11:35:27 +0100

    Add mutable systems

--- tests/system.rs
+++ tests/system.rs
@@ -17,3 +17,26 @@ fn remove() {
     world.remove_system(boom);
     world.run_systems();
 }
+
+#[test]
+fn remove_within() {
+    let mut world = World::default();
+    fn boom(_: &World) {
+        panic!()
+    }
+    // Miri will assume every time you mention a function you could have a new address
+    // While this is bogus in many cases, it's the only way to catch the rare cases where
+    // it's an issue. So we do this provenance preserving deduplication by eagerly creating a pointer.
+    let boom = boom as fn(&World);
+    world.add_resource(boom);
+
+    fn defuse(world: &mut World) {
+        let boom = *world.get_resource::<fn(&World)>().unwrap();
+        // Remove the boom system. Since we run before it, it will never actuall run, yay.
+        world.remove_system(boom);
+    }
+
+    world.add_mut_system(defuse);
+    world.add_system(boom, ExecutionMode::Serial);
+    world.run_systems();
+}
