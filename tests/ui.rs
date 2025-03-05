09040d1a89ea933f7d3877aff7bd1998769b405a
Oli Scherer <github@oli-obk.de>
2025-03-05 15:31:45 +0100

    Bless tests

--- tests/ui.rs
+++ tests/ui.rs
@@ -1,3 +1,5 @@
+use std::path::Path;
+
 use ui_test::{Config, dependencies::DependencyBuilder};
 
 fn main() -> ui_test::Result<()> {
@@ -9,6 +11,10 @@ fn main() -> ui_test::Result<()> {
     deps.crate_manifest_path = "ui_test_deps/Cargo.toml".into();
     config.comment_defaults.base().add_custom("deps", deps);
 
-    config.path_stderr_filter(&std::env::current_dir().unwrap(), "$DIR");
+    config.path_stderr_filter(&Path::new(file!()).parent().unwrap(), "$DIR");
+    config.stderr_filter("(src/.*\\.rs):[0-9]+:[0-9]+", "$1");
+    config.stderr_filter("[0-9][0-9][0-9] \\|", "LLL |");
+    config.stderr_filter("[0-9][0-9] \\|", "LL |");
+    config.stderr_filter("[0-9] \\|", "L |");
     ui_test::run_tests(config)
 }
