df802c29b59368f020890888b03d0a83ea51f823
Oli Scherer <github@oli-obk.de>
2025-03-06 16:58:39 +0100

    Add `query_retain` to remove entities while mutating them

--- tests/entity.rs
+++ tests/entity.rs
@@ -20,3 +20,13 @@ fn detach_unrelated_in_query() {
     world.spawn((10_u32, "foo"));
     world.query::<(&u32,)>(|entity, (_,)| world.detach::<&str>(entity));
 }
+
+#[test]
+fn detach_retain() {
+    let mut world = World::default();
+
+    world.spawn((1_u32,));
+    world.spawn((10_u32, "foo"));
+    world.query_retain::<u32>(|_, i| *i < 5);
+    world.query::<(&u32,)>(|_, (i,)| assert_eq!(*i, 1));
+}
