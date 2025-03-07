2d3513a910aa174890c9c30529cb8f6da51fac12
Oli Scherer <github@oli-obk.de>
2025-03-07 21:09:14 +0100

    Add `detach_all` and document everything

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,15 @@
+//! A very minimalistic entity component system.
+//!
+//! Start by creating a [World](world::World) and invoke methods on it
+//! to fill your world with life.
+
 mod components;
 mod query;
 mod scheduler;
 mod sparse_set;
 mod world;
 
+/// The one and only way to import things from this crate.
 pub mod prelude {
     pub use super::world::{Entity, World};
 }
