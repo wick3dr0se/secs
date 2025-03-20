90405b66dfaf63dd80f1448bb1c355d966722a01
Oli Scherer <noreply@oli-obk.de>
2025-03-20 19:35:51 +0100

    Add test for despawned entity access

---
+++ tests/ui/panic_location.rs
@@ -0,0 +1,13 @@
+//@ run: 101
+
+use secs::World;
+
+fn main() {
+    let mut world = World::default();
+
+    let id = world.spawn((1_u32,));
+    world.spawn((10_u32, "foo"));
+    world.despawn(id);
+
+    world.get::<u32>(id);
+}
